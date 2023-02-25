09f5032e2bf4d3ab2a6d08daff8062306a64f935
Andrew Vinicius <74573141+andrewvininius@users.noreply.github.com>
2023-02-24 23:22:58 -0300

    Add files via upload

--- Faturamento/Faturamento.py
+++ Faturamento/Faturamento.py
@@ -1,31 +1,24 @@
 import json
-from datetime import datetime
 
-# Lê o arquivo JSON com os dados de faturamento diário
-with open('faturamento.json') as f:
-    data = json.load(f)
+# Leitura dos dados do faturamento diário a partir de um arquivo json
+with open('faturamento.json', 'r') as file:
+    data = json.load(file)
 
-# Inicializa as variáveis para o menor e maior valor de faturamento
-menor_valor = float('inf')
-maior_valor = float('-inf')
+# Armazenamento dos valores de faturamento diário em um vetor
+faturamento_diario = data['faturamento']
 
-# Calcula o menor e o maior valor de faturamento diário
-for dia, valor in data.items():
-    if valor:
-        valor = float(valor)
-        if valor < menor_valor:
-            menor_valor = valor
-        if valor > maior_valor:
-            maior_valor = valor
+# Encontrar o menor e o maior valor de faturamento diário
+menor_valor = min(faturamento_diario)
+maior_valor = max(faturamento_diario)
 
-# Calcula a média mensal de faturamento, excluindo os dias sem faturamento
-valores = [float(v) for v in data.values() if v]
-media = sum(valores) / len(valores)
+# Calcular a média mensal de faturamento, ignorando os dias sem faturamento
+dias_com_faturamento = [faturamento for faturamento in faturamento_diario if faturamento > 0]
+media_mensal = sum(dias_com_faturamento) / len(dias_com_faturamento)
 
-# Conta o número de dias em que o valor de faturamento diário foi superior à média mensal
-dias_acima_da_media = sum(1 for v in valores if v > media)
+# Contar quantos dias tiveram faturamento diário superior à média mensal
+dias_acima_da_media = len([faturamento for faturamento in faturamento_diario if faturamento > media_mensal])
 
-# Imprime os resultados
-print(f'Menor valor de faturamento diário: R${menor_valor:.2f}')
-print(f'Maior valor de faturamento diário: R${maior_valor:.2f}')
-print(f'Número de dias acima da média mensal de faturamento: {dias_acima_da_media}')
+# Exibir os resultados
+print(f"Menor valor de faturamento diário: R${menor_valor:.2f}")
+print(f"Maior valor de faturamento diário: R${maior_valor:.2f}")
+print(f"Dias com faturamento diário acima da média: {dias_acima_da_media}")
